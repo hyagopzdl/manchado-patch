3ba6aafbb1cdd472b4db8fb3ba7c0bed1cc551e5
hyagopzdl <nascimentohyago@gmail.com>
2026-07-21 14:16:18 -0300

    Delete js directory

--- js/features/registry.js
+++
@@ -1,6 +0,0 @@
-(() => {
-  window.ManchaApp = window.ManchaApp || {};
-  window.ManchaApp.FEATURE_AREAS = Object.freeze([
-    "market", "roster", "matches", "championships", "reviews", "admin"
-  ]);
-})();
