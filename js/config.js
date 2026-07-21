2940c3658faa357df57d7efa06b88b7442e8ca43
hyagopzdl <nascimentohyago@gmail.com>
2026-07-21 01:11:38 -0300

    Delete js directory

--- js/config.js
+++
@@ -1,7 +0,0 @@
-// Copie este arquivo para o deploy e preencha apenas os valores públicos do projeto.
-// Nunca coloque SUPABASE_SERVICE_ROLE_KEY no frontend.
-window.__SUPABASE_CONFIG__ = {
-  url: "https://hoipranocxtertbjdxrx.supabase.co",
-  anonKey:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhvaXByYW5vY3h0ZXJ0YmpkeHJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1OTIyOTAsImV4cCI6MjEwMDE2ODI5MH0.P5vDmIgmDak5jhYGRTDsqec40UdzwqaNAo5hcE1DKik",
-};
